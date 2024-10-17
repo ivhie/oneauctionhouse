9913f132499d508c25c5941b3ad03dc9c367896a
Ivan M. Dolera <ivandolera24@gmail.com>
2024-10-17 09:28:42 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -259,14 +259,14 @@ Route::group(['namespace' => 'App\Http\Controllers'], function()
                 $message->to('ivandolera24@gmail.com')
                         ->from(env('FROM_EMAIL'), env('FROM_EMAIL_NAME'))
                          ->cc(['ivan_dolera@yahoo.com','benjietesting@gmail.com','ivandolera24@gmail.com','matthew.zy.lau@gmail.com'])
-                        //->bcc(['ivan_dolera@yahoo.com','benjietesting@gmail.com','ivandolera24@gmail.com','matthew.zy.lau@gmail.com'])
-                        ->subject('Test Email2 by Ivan')
+                         ->bcc(['ivan_dolera@yahoo.com','benjietesting@gmail.com','ivandolera24@gmail.com','matthew.zy.lau@gmail.com'])
+                        ->subject('Test Email2 by Ivan must send to gmail')
                         ->html('<h1>Hi Test Here!</h1><br/><br/><br/><p>This is a test <span style="color:red;">email</span> from Laravel using GoDaddy SMTP.</p>');
             });
 
-            Mail::to('ivandolera24@gmail.com')
-             ->cc(['ivan_dolera@yahoo.com','benjietesting@gmail.com','ivandolera24@gmail.com'])
-            ->send('testerererer');
+           // Mail::to('ivandolera24@gmail.com')
+             //->cc(['ivan_dolera@yahoo.com','benjietesting@gmail.com','ivandolera24@gmail.com'])
+            //->send('testerererer');
         
             return 'Test email sent2!';
         });
